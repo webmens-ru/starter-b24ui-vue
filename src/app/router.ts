56e1c8e6eff44c7ef7a61987f048adfdad1d137d
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2025-12-05 10:42:32 +0300

    Добавлен роут для мебели

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router"
 import Main from "../pages/Main.vue"
 import Form from "../pages/Form.vue"
+import FormFurniture from "../pages/FormFurniture.vue"
 
 const routes: RouteRecordRaw[] = [
   { path: '/', component: Main },
   { path: '/form', component: Form },
+  { path: '/formfurniture', component: FormFurniture },
 ]
 
 export const router = createRouter({
